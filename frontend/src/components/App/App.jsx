ee020d4ce7a3a60ccdd91de826b87ab71045c89d
Vika <zhuuvika@gmail.com>
2022-09-30 21:00:56 +0300

    add routers for reg, auth, userpage components

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -5,6 +5,9 @@ import Layout from '../Layout';
 import { loadAsyncUsers } from '../../storeAndSlices/Slices/usersReducer';
 import { loadAsyncBands } from '../../storeAndSlices/Slices/bandsReducer';
 import { loadAsyncSpots } from '../../storeAndSlices/Slices/spotsReducer';
+import UserPage from '../UserPage/UserPage';
+import Registraion from '../Registration/Registration';
+import Authorization from '../Authorization/Authorization';
 
 function App() {
   const dispatch = useDispatch();
@@ -20,6 +23,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* <Route path="/home" element={<HomePage />} /> */}
+          <Route path="/signup" element={<Registraion />} />
+          <Route path="/signin" element={<Authorization />} />
+          <Route path="/profile" element={<UserPage />} />
         </Route>
       </Routes>
     </div>
