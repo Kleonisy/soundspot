fad81bd9cd48387ac15f8152ea7dcddde876086f
Andrii Domanskyi <andromansky@gmail.com>
2022-09-30 13:57:54 +0300

    MainPage & some reducers little fixes

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Layout from '../Layout';
 import { loadAsyncUsers } from '../../storeAndSlices/Slices/usersReducer';
+import { loadAsyncBands } from '../../storeAndSlices/Slices/bandsReducer';
+import { loadAsyncSpots } from '../../storeAndSlices/Slices/spotsReducer';
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadAsyncUsers());
+    dispatch(loadAsyncBands());
+    dispatch(loadAsyncSpots());
   }, []);
 
   return (
