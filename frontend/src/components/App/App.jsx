092bb7e8f0e0985b79e63479bd2696dc83f35c02
Kleonisy <neron0195@mail.ru>
2022-09-30 15:41:18 +0300

    nav bar, navigation component, routing, ui elements, added react-icons library (package.json upgraded)

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -4,11 +4,13 @@ import Layout from '../Layout';
 
 function App() {
   return (
-    // <div className="App">
-    <Routes>
-      <Route path="/" element={<Layout />} />
-    </Routes>
-    // </div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route path="/home" element={<HomePage />} /> */}
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
---
+++ frontend/src/components/UI/SoundSpotLogo/SoundSpotLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import mySoundSpotLogo from './Logo.svg';
+
+function SoundSpotLogo() {
+  return (
+    <img style={{ width: '405px', heigth: '54px' }} src={mySoundSpotLogo} alt="SoundSpotLogo" />
+  );
+}
+
+export default SoundSpotLogo;
