f3cfcfd95ac57c9db5f5c134612ea01877b64715
Vika <zhuuvika@gmail.com>
2022-10-02 17:35:10 +0300

    update auth and users router

--- backend/routes/api/usersRouter.js
+++ backend/routes/api/usersRouter.js
@@ -1,11 +1,46 @@
 const express = require('express');
 
 const usersRouter = express.Router();
-const { User } = require('../../db/models');
+const {
+  User, UserInstrument, UserGenre, UserBand, Raiting,
+} = require('../../db/models');
 
 usersRouter.get('/', async (req, res) => {
-  const users = await User.findAll();
-  res.json({ users });
+  try {
+    const users = await User.findAll({
+      attributes: [
+        'id',
+        'login',
+        'email',
+        'about',
+        'latitude',
+        'longitude',
+        'contact',
+        'photo',
+      ],
+      include: [
+        User.Demo, User.Band,
+        {
+          model: UserInstrument,
+          include: UserInstrument.Instrument,
+        },
+        {
+          model: UserGenre,
+          include: UserGenre.Genre,
+        },
+        {
+          model: UserBand,
+          include: UserBand.Band,
+        },
+        {
+          model: Raiting,
+        },
+      ],
+    });
+    res.json({ users });
+  } catch (error) {
+    res.json(error.message);
+  }
 });
 
 module.exports = usersRouter;
