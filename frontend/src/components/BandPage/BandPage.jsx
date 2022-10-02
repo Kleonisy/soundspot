e62a1357381ac747da7f71b51156961b366dad57
KrAnatoly <kranatoly@gmail.com>
2022-10-02 14:02:38 +0300

    create BandPage.jsx

--- frontend/src/components/BandPage/BandPage.jsx
+++ frontend/src/components/BandPage/BandPage.jsx
@@ -1,9 +1,35 @@
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadAsyncBand } from '../../storeAndSlices/Slices/bandsReducer';
 
 function BandPage() {
-    return (
-    
-)
+  const { band } = useSelector((state) => state.bandsState);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadAsyncBand());
+  }, []);
+
+  return (
+    <div className="band-page">
+      <div className="band-page-left">
+        <div className="band-name">{band.name}</div>
+        <div className="band-photo">{band.photo}</div>
+        <div className="band-about">{band.about}</div>
+      </div>
+      <div className="band-page-right">
+        <div className="band-members">
+          Members
+          { }
+        </div>
+        <div className="band-checkout-demos">
+          Checkout our demos
+          <Link />
+        </div>
+        <button>Move Back</button>
+      </div>
+    </div>
+  );
 }
 
-export default BandPage
+export default BandPage;
