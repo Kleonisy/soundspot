94114cdeb397e27a39a1a990d6f2b67c4918a28f
Vika <zhuuvika@gmail.com>
2022-10-03 15:40:18 +0300

    fix user raiting

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,67 +1,59 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import Rating from '../UI/Rating/Rating';
-import '../UserPage/UserPage.css';
+import './Profile.css';
 
 function Profile() {
   const { data: user, hasUser } = useSelector((state) => state.authState);
 
   return (
-    <div className="user-container">
-      <div className="left-container">
-        <div className="name-rating">
+    <div className="profile-container">
+      <div className="left-prof-box">
+        <div className="name-rating-box">
           <h2>{hasUser && user.login}</h2>
-          <Rating />
+          <Rating user={user && user} />
         </div>
-        <div className="upper-inf-container">
+        <div className="upper-inf-box">
           <div className="inf-box">
             <h5>My genres</h5>
-            <p className="user-inf">
+            <p className="profile-inf">
               {hasUser
           && user.UserGenres.length > 0
-          && user.UserGenres.map((data) => <p className="user-inf" key={data.id}>{data.Genre.genre}</p>)}
+          && user.UserGenres.map((data) => <p className="profile-inf" key={data.id}>{data.Genre.genre}</p>)}
             </p>
           </div>
           <div className="inf-box">
             <h5>My Instruments</h5>
-            <p className="user-inf">
+            <p className="profile-inf">
               {hasUser
             && user.UserInstruments.length > 0
-            && user.UserInstruments.map((data) => <p className="user-inf" key={data.id}>{data.Instrument.instrument}</p>)}
+            && user.UserInstruments.map((data) => <p className="profile-inf" key={data.id}>{data.Instrument.instrument}</p>)}
             </p>
           </div>
           <div className="inf-box">
             <h5>My Bands</h5>
-            <p className="user-inf">
+            <p className="profile-inf">
               {user.UserBands
             && user.UserBands.length > 0
-            && user.UserBands.map((data) => <p className="user-inf" key={data.id}>{data.Band.name}</p>)}
+            && user.UserBands.map((data) => <p className="profile-inf" key={data.id}>{data.Band.name}</p>)}
             </p>
           </div>
           <div className="inf-box">
             <h5>Contact me:</h5>
-            <p className="user-inf">
+            <p className="profile-inf">
               {hasUser && user.contact}
             </p>
           </div>
         </div>
-        <div className="lower-inf-container">
+        <div className="lower-inf-box">
           <div>
             <h5>About</h5>
-            <p className="user-inf">{hasUser && user.about}</p>
-          </div>
-          <div>
-            <h5>
-              Check out my demos
-              {' '}
-              <NavLink to={hasUser && `/users/${user.id}/demos`}>here</NavLink>
-            </h5>
+            <p className="profile-inf">{hasUser && user.about}</p>
           </div>
         </div>
       </div>
-      <div className="right-container">
-        {hasUser && <div className="user-img-container"><img className="user-img" src={user.photo} alt={user.login} /></div>}
+      <div className="right-box">
+        {hasUser && <div className="profile-img-box"><img className="user-img" src={user.photo} alt={user.login} /></div>}
       </div>
     </div>
   );
--- frontend/src/components/UserPage/UserPage.jsx
+++ frontend/src/components/UserPage/UserPage.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useParams } from 'react-router-dom';
+import { findUser } from '../../storeAndSlices/Slices/usersReducer';
+import Rating from '../UI/Rating/Rating';
 import './UserPage.css';
 
 function UserPage() {
-  const { data: user, hasUser } = useSelector((state) => state.authState);
+  const { user, usersData } = useSelector((state) => state.usersState);
+  const dispatch = useDispatch();
+  const { id } = useParams();
 
-  // const userRaiting = (currUser) => {
-  //   const total = currUser.Raitings.map((el) => el.raiting);
-  //   return total.reduce((curr, prev) => (curr + prev)) / total.length;
-  // };
-
-  // const raiting = useMemo(() => userRaiting(user), []);
+  useEffect(() => {
+    if (usersData.length) {
+      dispatch(findUser(Number(id)));
+    }
+  }, [id, usersData]);
 
   return (
     <div className="user-container">
       <div className="left-container">
-        <h2>{hasUser && user.login}</h2>
+        <div className="rating-box">
+          <h2>{user && user.login}</h2>
+          <Rating user={user} />
+        </div>
         <div className="upper-inf-container">
           <div className="inf-box">
             <h5>My genres</h5>
             <p className="user-inf">
-              {hasUser
+              {user
           && user.UserGenres.length > 0
           && user.UserGenres.map((data) => <p className="user-inf" key={data.id}>{data.Genre.genre}</p>)}
             </p>
@@ -29,7 +35,7 @@ function UserPage() {
           <div className="inf-box">
             <h5>My Instruments</h5>
             <p className="user-inf">
-              {hasUser
+              {user
             && user.UserInstruments.length > 0
             && user.UserInstruments.map((data) => <p className="user-inf" key={data.id}>{data.Instrument.instrument}</p>)}
             </p>
@@ -37,7 +43,7 @@ function UserPage() {
           <div className="inf-box">
             <h5>My Bands</h5>
             <p className="user-inf">
-              {user.UserBands
+              {user
             && user.UserBands.length > 0
             && user.UserBands.map((data) => <p className="user-inf" key={data.id}>{data.Band.name}</p>)}
             </p>
@@ -45,26 +51,26 @@ function UserPage() {
           <div className="inf-box">
             <h5>Contact me:</h5>
             <p className="user-inf">
-              {hasUser && user.contact}
+              {user && user.contact}
             </p>
           </div>
         </div>
         <div className="lower-inf-container">
           <div>
             <h5>About</h5>
-            <p className="user-inf">{hasUser && user.about}</p>
+            <p className="user-inf">{user && user.about}</p>
           </div>
           <div>
             <h5>
               Check out my demos
               {' '}
-              <NavLink to={hasUser && `/users/${user.id}/demos`}>here</NavLink>
+              <NavLink to={user && `/users/${user.id}/music`}>here</NavLink>
             </h5>
           </div>
         </div>
       </div>
       <div className="right-container">
-        {hasUser && <div className="user-img-container"><img className="user-img" src={user.photo} alt={user.login} /></div>}
+        {user && <div className="user-img-container"><img className="user-img" src={user.photo} alt={user.login} /></div>}
       </div>
     </div>
   );
