ad4bebc4d4e86fc8078353832ac0b8757b9121f7
Andrii Domanskyi <andromansky@gmail.com>
2022-10-03 10:11:19 +0300

    Player feature added

--- frontend/src/components/UserPage/UserPage.jsx
+++ frontend/src/components/UserPage/UserPage.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import './UserPage.css';
