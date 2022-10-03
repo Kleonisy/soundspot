4809eb6944063cc86466feec923333c62008ca39
Kleonisy <neron0195@mail.ru>
2022-10-03 10:16:27 +0300

    useEffect add to cart

--- frontend/src/components/SpotsSearchPage/SpotsSearchPage.jsx
+++ frontend/src/components/SpotsSearchPage/SpotsSearchPage.jsx
@@ -1,49 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './SpotsSearchPage.css';
 
 function SpotsSearchPage() {
   const { spots } = useSelector((store) => store.spotsState);
-  if (spots.length) {
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  useEffect(() => {
     let myMap;
-
-    // eslint-disable-next-line no-undef
-    ymaps.ready(() => {
+    if (spots.length) {
       // eslint-disable-next-line no-undef
-      myMap = new ymaps.Map('first_map', {
-        center: [59.94, 30.32],
-        zoom: 12
-      });
-      const spotsArray = spots.map((spot) => (
+      ymaps.ready(() => {
         // eslint-disable-next-line no-undef
-        new ymaps.Placemark([spot.dataValues.latitude, spot.dataValues.longitude],
-          {
-            iconCaption: spot.dataValues.name,
-            balloonContentHeader: spot.dataValues.name,
-            balloonContentBody: [
-              spot.dataValues.description,
-              `<br>See <a href="http://localhost:3000/spots/${spot.dataValues.id}">more...</a>`
-            ].join(''),
-          },
-          {
-            preset: 'islands#blackStarCircleIcon',
-          }
-        )
-      ));
-      // eslint-disable-next-line no-undef
-      const objects = ymaps.geoQuery(spotsArray);
-      objects.searchInside(myMap).addToMap(myMap);
+        myMap = new ymaps.Map('first_map', {
+          center: [59.94, 30.32],
+          zoom: 12
+        });
+        const spotsArray = spots.map((spot) => (
+          // eslint-disable-next-line no-undef
+          new ymaps.Placemark([spot.dataValues.latitude, spot.dataValues.longitude],
+            {
+              iconCaption: spot.dataValues.name,
+              balloonContentHeader: spot.dataValues.name,
+              balloonContentBody: [
+                spot.dataValues.description,
+                `<br>See <a href="http://localhost:3000/spots/${spot.dataValues.id}">more...</a>`
+              ].join(''),
+            },
+            {
+              preset: 'islands#blackStarCircleIcon',
+            }
+          )
+        ));
+        // eslint-disable-next-line no-undef
+        const objects = ymaps.geoQuery(spotsArray);
+        objects.searchInside(myMap).addToMap(myMap);
 
-      myMap.events.add('boundschange', () => {
-        const visibleObjects = objects.searchInside(myMap).addToMap(myMap);
-        objects.remove(visibleObjects).removeFromMap(myMap);
+        myMap.events.add('boundschange', () => {
+          const visibleObjects = objects.searchInside(myMap).addToMap(myMap);
+          objects.remove(visibleObjects).removeFromMap(myMap);
+        });
+        return () => {
+          myMap.destroy();
+        };
       });
-    });
-  }
+    }
+  }, [spots]);
 
   return (
     <div className="soundSpot__spotsSearch-container">
-      <input placeholder="Search..." />
+      <input value={filter.query} onChange={(event) => setFilter({ ...filter, query: event.target.value })} placeholder="Search..." />
       <div className="soundSpot__spotsSearch_content-container">
         <div className="soundSpot__spotsSearch_results-container">
           {
