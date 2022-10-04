919c49c7d3614b926bed7b0c3c9a2213926524fd
Kleonisy <neron0195@mail.ru>
2022-10-04 22:43:52 +0300

    userProfile and other users page styles and jsx correction

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState, useEffect } from 'react';
@@ -22,54 +23,56 @@ function Profile() {
     <div className="profile-container">
       <div className="left-prof-box">
         <div className="name-rating-box">
-          <h2>{hasUser && user.login}</h2>
+          <div className="soundSpot__username">{hasUser && user.login}</div>
           <div className="soundSpot_rating-container">
             <Rating user={user && user} />
           </div>
+          <div type="button" className="soundSpot__editProfileTag" onClick={() => setModalShow(true)}>Edit Profile</div>
+          {modalShow && <UserEditProfile onHide={() => setModalShow(false)} />}
         </div>
-        <div className="upper-inf-box">
-          <div className="inf-box">
-            <h5>My genres</h5>
-            <p className="profile-inf">
-              {hasUser
-                && user.UserGenres.length > 0
-                && user.UserGenres.map((data) => <p className="profile-inf" key={data.id}>{data.Genre.genre}</p>)}
-            </p>
+        <div className="soundSpot_profileInfo-container">
+          <div className="upper-inf-box">
+            <div className="inf-box">
+              <h5>My genres</h5>
+              <p className="profile-inf">
+                {hasUser
+                  && user.UserGenres.length > 0
+                  && user.UserGenres.map((data) => <p className="profile-inf" key={data.id}>{data.Genre.genre}</p>)}
+              </p>
+            </div>
+            <div className="inf-box">
+              <h5>My Instruments</h5>
+              <p className="profile-inf">
+                {hasUser
+                  && user.UserInstruments.length > 0
+                  && user.UserInstruments.map((data) => <p className="profile-inf" key={data.id}>{data.Instrument.instrument}</p>)}
+              </p>
+            </div>
+            <div className="inf-box">
+              <h5>My Bands</h5>
+              <p className="profile-inf">
+                {user.UserBands
+                  && user.UserBands.length > 0
+                  && user.UserBands.map((data) => <p className="profile-inf" key={data.id}>{data.Band.name}</p>)}
+              </p>
+            </div>
+            <div className="inf-box">
+              <h5>Contact me:</h5>
+              <p className="profile-inf">
+                {hasUser && user.contact}
+              </p>
+            </div>
           </div>
-          <div className="inf-box">
-            <h5>My Instruments</h5>
-            <p className="profile-inf">
-              {hasUser
-                && user.UserInstruments.length > 0
-                && user.UserInstruments.map((data) => <p className="profile-inf" key={data.id}>{data.Instrument.instrument}</p>)}
-            </p>
-          </div>
-          <div className="inf-box">
-            <h5>My Bands</h5>
-            <p className="profile-inf">
-              {user.UserBands
-                && user.UserBands.length > 0
-                && user.UserBands.map((data) => <p className="profile-inf" key={data.id}>{data.Band.name}</p>)}
-            </p>
-          </div>
-          <div className="inf-box">
-            <h5>Contact me:</h5>
-            <p className="profile-inf">
-              {hasUser && user.contact}
-            </p>
-          </div>
-        </div>
-        <div className="lower-inf-box">
-          <div className="soundSpot_progile-about">
-            <h5>About</h5>
-            <p className="profile-inf">{hasUser && user.about}</p>
+          <div className="lower-inf-box">
+            <div className="soundSpot_profile-about">
+              <h5>About</h5>
+              <p className="profile-inf">{hasUser && user.about}</p>
+            </div>
           </div>
         </div>
       </div>
       <div className="right-box">
         {hasUser && <div className="profile-img-box"><img className="user-img" src={user.photo} alt={user.login} /></div>}
-        <h4 type="button" className="editProfileTag" onClick={() => setModalShow(true)}>Edit Profile</h4>
-        {modalShow && <UserEditProfile onHide={() => setModalShow(false)} />}
       </div>
     </div>
   );
--- frontend/src/components/UserPage/UserPage.jsx
+++ frontend/src/components/UserPage/UserPage.jsx
@@ -21,39 +21,43 @@ function UserPage() {
     <div className="user-container">
       <div className="left-container">
         <div className="rating-box">
-          <h2>{user && user.login}</h2>
-          <Rating user={user} />
+          <div className="soundSpot__username-userPage">{user && user.login}</div>
+          <div className="soundSpot_rating-container">
+            <Rating user={user} />
+          </div>
         </div>
-        <div className="upper-inf-container">
-          <div className="inf-box">
-            <h5>My genres</h5>
-            <p className="user-inf">
-              {user
-          && user.UserGenres
-          && user.UserGenres.map((data) => <p className="user-inf" key={data.id}>{data.Genre.genre}</p>)}
-            </p>
-          </div>
-          <div className="inf-box">
-            <h5>My Instruments</h5>
-            <p className="user-inf">
-              {user
-            && user.UserInstruments
-            && user.UserInstruments.map((data) => <p className="user-inf" key={data.id}>{data.Instrument.instrument}</p>)}
-            </p>
-          </div>
-          <div className="inf-box">
-            <h5>My Bands</h5>
-            <p className="user-inf">
-              {user
-            && user.UserBands
-            && user.UserBands.map((data) => <p className="user-inf" key={data.id}>{data.Band.name}</p>)}
-            </p>
-          </div>
-          <div className="inf-box">
-            <h5>Contact me:</h5>
-            <p className="user-inf">
-              {user && user.contact}
-            </p>
+        <div className="soundSpot_userInfo-container">
+          <div className="upper-inf-container">
+            <div className="inf-box">
+              <h5>My genres</h5>
+              <p className="user-inf">
+                {user
+                  && user.UserGenres
+                  && user.UserGenres.map((data) => <p className="user-inf" key={data.id}>{data.Genre.genre}</p>)}
+              </p>
+            </div>
+            <div className="inf-box">
+              <h5>My Instruments</h5>
+              <p className="user-inf">
+                {user
+                  && user.UserInstruments
+                  && user.UserInstruments.map((data) => <p className="user-inf" key={data.id}>{data.Instrument.instrument}</p>)}
+              </p>
+            </div>
+            <div className="inf-box">
+              <h5>My Bands</h5>
+              <p className="user-inf">
+                {user
+                  && user.UserBands
+                  && user.UserBands.map((data) => <p className="user-inf" key={data.id}>{data.Band.name}</p>)}
+              </p>
+            </div>
+            <div className="inf-box">
+              <h5>Contact me:</h5>
+              <p className="user-inf">
+                {user && user.contact}
+              </p>
+            </div>
           </div>
         </div>
         <div className="lower-inf-container">
@@ -61,17 +65,15 @@ function UserPage() {
             <h5>About</h5>
             <p className="user-inf">{user && user.about}</p>
           </div>
-          <div>
-            <h5>
-              Check out my demos
-              {' '}
-              <NavLink to={user && `/artists/${user.id}/music`}>here</NavLink>
-            </h5>
-          </div>
         </div>
       </div>
       <div className="right-container">
         {user && <div className="user-img-container"><img className="user-img" src={user.photo} alt={user.login} /></div>}
+        <div className="soundSpot__checkMyDemos">
+          Check out my demos
+          {' '}
+          <NavLink className="soundSpot__checkMyDemos_link" to={user && `/artists/${user.id}/music`}>here</NavLink>
+        </div>
       </div>
     </div>
   );
