3812265fa6ee0f0971e8264449cfae36c183ae08
Vika <zhuuvika@gmail.com>
2022-10-04 15:18:03 +0300

    change userdemo to demos

--- frontend/src/components/Demos/Demos.jsx
+++ frontend/src/components/Demos/Demos.jsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { loadAsyncBand } from '../../storeAndSlices/Slices/bandsReducer';
 import { loadUser } from '../../storeAndSlices/Slices/userReducer';
 import Demo from '../Demo/Demo';
 import Player from '../Player/Player';
-import './UserDemo.css';
+import './Demos.css';
 
-function UserDemo() {
+function Demos() {
   const [value, setValue] = useState('');
   const [player, setPlayer] = useState(false);
   const { id } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const { pathname } = useLocation();
   const { user } = useSelector((state) => state.userState);
+  const { band } = useSelector((state) => state.bandsState);
   const demos = [{ id: 1, songName: 'nnn' }, { id: 2, songName: 'nnn' }, { id: 3, songName: 'nnn' }, { id: 4, songName: 'nmd' }, { id: 5, songName: 'nmmd' },
     { id: 6, songName: 'nnn' }, { id: 7, songName: 'nnn' }, { id: 8, songName: 'nnn' }, { id: 9, songName: 'nmd' }, { id: 10, songName: 'nmmd' }];
 
   useEffect(() => {
-    dispatch(loadUser(id));
-  }, []);
+    if (pathname.includes('bands')) {
+      dispatch(loadAsyncBand(id));
+    } else {
+      dispatch(loadUser(id));
+    }
+  }, [pathname]);
 
   return (
     <div className="user-demo-cont">
@@ -30,11 +38,13 @@ function UserDemo() {
           {demos
             && demos
               .filter((demo) => demo.songName.toLowerCase().includes(value.toLowerCase()))
-              .map((demo) => <Demo key={demo.id} demo={demo} owner={user} setPlayer={setPlayer} />)}
+              .map((demo) =>
+                <Demo key={demo.id} demo={demo} owner={pathname.includes('bands') ? band : user} setPlayer={setPlayer} />
+              )}
         </div>
       </div>
     </div>
   );
 }
 
-export default UserDemo;
+export default Demos;
